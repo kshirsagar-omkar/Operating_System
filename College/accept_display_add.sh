a9dd74f0ff83d27919fd6ebeace356b3be15ab4f
Omkar21-1 <omkarkshirsagar2005@gmail.com>
2024-08-01 22:59:50 +0530

    College Practicle

---
+++ College/accept_display_add.sh
@@ -0,0 +1,18 @@
+#! bin/bash
+
+echo "Hello Programmer\n"
+
+
+printf  "Enter First Number : "
+read n1
+
+printf "Enter Second Number : "
+read n2
+
+sum=$((n1+n2))
+
+#sum1=$(('expr $a+$b'))
+
+echo "Addition :" $sum #$sum1
+
+
